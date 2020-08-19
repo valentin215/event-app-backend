f43a3e484a7cf782221e515d141f77a0ff1b5ca0
Valentin Birault <biraultvalentin@gmail.com>
2020-08-19 16:05:37 +0200

    before dropping

--- app/models/custom_attribute.rb
+++ app/models/custom_attribute.rb
@@ -1,3 +1,24 @@
+# frozen_string_literal: true
+
+# CustomAttribue class, notice that it can belongs_to event or not.
+# Depends if the admin decide to create the custom attribute
+# for a User (for_user column) or for an Event (for_event_registration column)
 class CustomAttribute < ApplicationRecord
-  belongs_to :event
+  belongs_to :event, optional: true
+
+  validates_presence_of :name
+
+  validates :for_user,
+            :for_event_registration,
+            :required_for_signup,
+            :required_for_profile,
+            :required_for_event_form, inclusion: { in: [true, false] }
+
+  validates :attribute_type, presence: true, inclusion: { in: %w[string boolean] }
+
+  validates :event, presence: true, if: -> { :event_custom_attribute? }
+
+  def event_custom_attribute?
+    for_event_registration
+  end
 end
--- db/migrate/20200819101607_create_custom_attributes.rb
+++ db/migrate/20200819101607_create_custom_attributes.rb
@@ -6,9 +6,9 @@ class CreateCustomAttributes < ActiveRecord::Migration[6.0]
       t.boolean :for_event_registration, default: false, null: false
       t.boolean :required_for_signup, default: false, null: false
       t.boolean :required_for_profile, default: false, null: false
-      t.boolean :required_for_event, default: false, null: false
+      t.boolean :required_for_event_form, default: false, null: false
       t.string :attribute_type, null: false
-      t.references :event, null: false, foreign_key: true
+      t.references :event, foreign_key: true
 
       t.timestamps
     end
